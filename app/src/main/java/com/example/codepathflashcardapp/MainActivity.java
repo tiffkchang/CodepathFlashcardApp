8d2c64f9ce8abd396435241068bfc2019d9e909c
tiffkchang <tiffkchang@gmail.com>
2021-02-20 10:59:55 -0800

    basic flashcard functionality implemented

--- app/src/main/java/com/example/codepathflashcardapp/MainActivity.java
+++ app/src/main/java/com/example/codepathflashcardapp/MainActivity.java
@@ -3,6 +3,10 @@ package com.example.codepathflashcardapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final TextView flashcardQuestion = findViewById(R.id.flashcardQuestion);
+        final TextView flashcardAnswer = findViewById(R.id.flashcardAnswer);
+
+        flashcardQuestion.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                v.setVisibility(View.INVISIBLE);
+                flashcardAnswer.setVisibility(View.VISIBLE);
+            }
+        });
+
+        flashcardAnswer.setOnClickListener((new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                v.setVisibility(View.INVISIBLE);
+                flashcardQuestion.setVisibility(View.VISIBLE);
+            }
+        }));
     }
 }
