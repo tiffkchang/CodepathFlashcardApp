0e8b5ed6eb1ee5a7477d3fedc00db36bd3d00f66
tiffkchang <tiffkchang@gmail.com>
2021-03-13 12:52:09 -0800

    all lab 3 required + optional stories completed

--- README.md
+++ README.md
@@ -1,26 +1,23 @@
 ## CodepathFlashcardApp
 
-## Lab 1
+## Lab 3
 
 ### App Description
-This flashcard app allows the user to view a question and its answer by tapping on the card. Users can also pick one of the multiple choice options and receive immediate feedback on whether their answer was correct or not.
+This app allows the user to add and edit multiple flashcard as well as cycle through them. Persistent storage has also been added; when the user exits and reopens the app, their previous flashcards will still be there. Pressing the shuffle button will randomize the order of the flashcards.
 
 ### App Walk-though
 
-<img src="https://github.com/tiffkchang/CodepathFlashcardApp/blob/main/Demo%20Gifs/CodepathFlashcardAppLab1Gif2.gif" width=200><br>
-
+<img src="YOUR_GIF_URL_HERE" width=200><br>
 
 ## Required
-- [x] Create New Project in Android Studio
-- [x] Add a view for the front side of the flashcard to display the question
-- [x] Add a view for the back side of the flashcard to display the answer
-- [x] Build in logic to show the answer side when the card is tapped
-- [x] Push code to GitHub
+- [x] User can create a card and still see their created card when the app is relaunched.
+- [x] User can create multiple cards and browse through all created cards
+
 ## Optional
-- [x] toggle the flashcard between the question side and the answer side
-- [x] Style the question and answer side of the card to better distinguish between the two sides
-- [x] Add selectable multiple choice answers beneath the card
-   - [x] Change the background color of the multiple choice answers when clicked to indicate whether the question was answered correctly
+- [x] User can delete a card and no longer see it in their deck anymore
+- [x] Flashcards are shown in random order instead of the order they were created in
+- [x] User can edit a card and see the edit saved when they browse through their deck of cards
+- [x] User can create flashcards with multiple answers and be able to browse through cards with multiple choice answers shown
 
 ------------
 
@@ -46,3 +43,27 @@ This app allows the user to create a new flashcard, input a question and an answ
 - [x] A notification is shown if the card was created successfully
 - [x] User can also enter multiple choice answers when creating a card
 - [x] App is further styled and customized!
+
+------------
+
+## Lab 1
+
+### App Description
+This flashcard app allows the user to view a question and its answer by tapping on the card. Users can also pick one of the multiple choice options and receive immediate feedback on whether their answer was correct or not.
+
+### App Walk-though
+
+<img src="https://github.com/tiffkchang/CodepathFlashcardApp/blob/main/Demo%20Gifs/CodepathFlashcardAppLab1Gif2.gif" width=200><br>
+
+
+## Required
+- [x] Create New Project in Android Studio
+- [x] Add a view for the front side of the flashcard to display the question
+- [x] Add a view for the back side of the flashcard to display the answer
+- [x] Build in logic to show the answer side when the card is tapped
+- [x] Push code to GitHub
+## Optional
+- [x] toggle the flashcard between the question side and the answer side
+- [x] Style the question and answer side of the card to better distinguish between the two sides
+- [x] Add selectable multiple choice answers beneath the card
+   - [x] Change the background color of the multiple choice answers when clicked to indicate whether the question was answered correctly
--- app/src/main/java/com/example/codepathflashcardapp/MainActivity.java
+++ app/src/main/java/com/example/codepathflashcardapp/MainActivity.java
@@ -7,19 +7,30 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.material.snackbar.Snackbar;
 
 import org.w3c.dom.Text;
 
+import java.util.Collections;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
 
+    FlashcardDatabase flashcardDatabase;
+    List<Flashcard> allFlashcards;
+    int currentCardDisplayedIndex = 0;
     private final int REQUEST_CODE = 20;
+    private final int EDIT_REQUEST_CODE = 30;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        flashcardDatabase = new FlashcardDatabase(getApplicationContext());
+        allFlashcards = flashcardDatabase.getAllCards();
 
         final TextView flashcardQuestion = findViewById(R.id.flashcardQuestion);
         final TextView flashcardAnswer = findViewById(R.id.flashcardAnswer);
@@ -30,6 +41,24 @@ public class MainActivity extends AppCompatActivity {
 
         ImageView addFlashcard = findViewById(R.id.plus_icon);
         ImageView editFlashcard = findViewById(R.id.edit_icon);
+        ImageView nextButton = findViewById(R.id.next_icon);
+        ImageView backButton = findViewById(R.id.back_icon);
+        ImageView deleteButton = findViewById(R.id.delete_icon);
+        ImageView shuffleButton = findViewById(R.id.shuffle_icon);
+
+        if (allFlashcards.size() > 0) {
+            flashcardQuestion.setText(allFlashcards.get(currentCardDisplayedIndex).getQuestion());
+            flashcardAnswer.setText(allFlashcards.get(currentCardDisplayedIndex).getAnswer());
+            flashcardAnswer1.setText(allFlashcards.get(currentCardDisplayedIndex).getWrongAnswer1());
+            flashcardAnswer2.setText(allFlashcards.get(currentCardDisplayedIndex).getAnswer());
+            flashcardAnswer3.setText(allFlashcards.get(currentCardDisplayedIndex).getWrongAnswer2());
+        } else {
+            flashcardQuestion.setText("");
+            flashcardAnswer.setText("");
+            flashcardAnswer1.setText("");
+            flashcardAnswer2.setText("");
+            flashcardAnswer3.setText("");
+        }
 
         flashcardQuestion.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -93,10 +122,85 @@ public class MainActivity extends AppCompatActivity {
                 intent.putExtra("answer", flashcardAnswer.getText().toString());
                 intent.putExtra("wrongAnswer1", flashcardAnswer1.getText().toString());
                 intent.putExtra("wrongAnswer2", flashcardAnswer3.getText().toString());
-                MainActivity.this.startActivityForResult(intent, REQUEST_CODE);
+                MainActivity.this.startActivityForResult(intent, EDIT_REQUEST_CODE);
                 flashcardAnswer1.setBackgroundColor(getResources().getColor(R.color.answerOutline));
                 flashcardAnswer2.setBackgroundColor(getResources().getColor(R.color.answerOutline));
                 flashcardAnswer3.setBackgroundColor(getResources().getColor(R.color.answerOutline));
+            }
+        });
+
+        nextButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (currentCardDisplayedIndex + 1 < allFlashcards.size()) {
+                    currentCardDisplayedIndex++;
+                    flashcardQuestion.setText(allFlashcards.get(currentCardDisplayedIndex).getQuestion());
+                    flashcardAnswer.setText(allFlashcards.get(currentCardDisplayedIndex).getAnswer());
+                    flashcardAnswer1.setText(allFlashcards.get(currentCardDisplayedIndex).getWrongAnswer1());
+                    flashcardAnswer2.setText(allFlashcards.get(currentCardDisplayedIndex).getAnswer());
+                    flashcardAnswer3.setText(allFlashcards.get(currentCardDisplayedIndex).getWrongAnswer2());
+                } else {
+                    Snackbar.make(findViewById(R.id.flashcardQuestion),
+                            "This is the last flashcard.",
+                            Snackbar.LENGTH_SHORT)
+                            .show();
+                }
+            }
+        });
+
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (currentCardDisplayedIndex - 1 > -1) {
+                    currentCardDisplayedIndex--;
+                    flashcardQuestion.setText(allFlashcards.get(currentCardDisplayedIndex).getQuestion());
+                    flashcardAnswer.setText(allFlashcards.get(currentCardDisplayedIndex).getAnswer());
+                    flashcardAnswer1.setText(allFlashcards.get(currentCardDisplayedIndex).getWrongAnswer1());
+                    flashcardAnswer2.setText(allFlashcards.get(currentCardDisplayedIndex).getAnswer());
+                    flashcardAnswer3.setText(allFlashcards.get(currentCardDisplayedIndex).getWrongAnswer2());
+                } else {
+                    Snackbar.make(findViewById(R.id.flashcardQuestion),
+                            "This is the first flashcard.",
+                            Snackbar.LENGTH_SHORT)
+                            .show();
+                }
+            }
+        });
+
+        deleteButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                flashcardDatabase.deleteCard(allFlashcards.get(currentCardDisplayedIndex).getQuestion());
+                allFlashcards = flashcardDatabase.getAllCards();
+                if (currentCardDisplayedIndex - 1 > -1) {
+                    currentCardDisplayedIndex--;
+                    flashcardQuestion.setText(allFlashcards.get(currentCardDisplayedIndex).getQuestion());
+                    flashcardAnswer.setText(allFlashcards.get(currentCardDisplayedIndex).getAnswer());
+                    flashcardAnswer1.setText(allFlashcards.get(currentCardDisplayedIndex).getWrongAnswer1());
+                    flashcardAnswer2.setText(allFlashcards.get(currentCardDisplayedIndex).getAnswer());
+                    flashcardAnswer3.setText(allFlashcards.get(currentCardDisplayedIndex).getWrongAnswer2());
+                } else {
+                    flashcardQuestion.setText("");
+                    flashcardAnswer.setText("");
+                    flashcardAnswer1.setText("");
+                    flashcardAnswer2.setText("");
+                    flashcardAnswer3.setText("");
+                }
+            }
+        });
+
+        shuffleButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (allFlashcards.size() > 0) {
+                    Collections.shuffle(allFlashcards);
+                    currentCardDisplayedIndex = 0;
+                    flashcardQuestion.setText(allFlashcards.get(currentCardDisplayedIndex).getQuestion());
+                    flashcardAnswer.setText(allFlashcards.get(currentCardDisplayedIndex).getAnswer());
+                    flashcardAnswer1.setText(allFlashcards.get(currentCardDisplayedIndex).getWrongAnswer1());
+                    flashcardAnswer2.setText(allFlashcards.get(currentCardDisplayedIndex).getAnswer());
+                    flashcardAnswer3.setText(allFlashcards.get(currentCardDisplayedIndex).getWrongAnswer2());
+                }
             }
         });
     }
@@ -107,29 +211,39 @@ public class MainActivity extends AppCompatActivity {
             return;
         }
 
-        if (requestCode == 20) {
-            final TextView flashcardQuestion = findViewById(R.id.flashcardQuestion);
-            final TextView flashcardAnswer = findViewById(R.id.flashcardAnswer);
+        final TextView flashcardQuestion = findViewById(R.id.flashcardQuestion);
+        final TextView flashcardAnswer = findViewById(R.id.flashcardAnswer);
 
-            final TextView flashcardAnswer1 = findViewById(R.id.flashcardAnswer1);
-            final TextView flashcardAnswer2 = findViewById(R.id.flashcardAnswer2);
-            final TextView flashcardAnswer3 = findViewById(R.id.flashcardAnswer3);
+        final TextView flashcardAnswer1 = findViewById(R.id.flashcardAnswer1);
+        final TextView flashcardAnswer2 = findViewById(R.id.flashcardAnswer2);
+        final TextView flashcardAnswer3 = findViewById(R.id.flashcardAnswer3);
 
-            String question = data.getExtras().getString("question");
-            String answer = data.getExtras().getString("answer");
-            String wrongAnswer1 = data.getExtras().getString("wrongAnswer1");
-            String wrongAnswer2 = data.getExtras().getString("wrongAnswer2");
+        String question = data.getExtras().getString("question");
+        String answer = data.getExtras().getString("answer");
+        String wrongAnswer1 = data.getExtras().getString("wrongAnswer1");
+        String wrongAnswer2 = data.getExtras().getString("wrongAnswer2");
 
-            flashcardQuestion.setText(question);
-            flashcardAnswer.setText(answer);
-            flashcardAnswer1.setText(wrongAnswer1);
-            flashcardAnswer2.setText(answer);
-            flashcardAnswer3.setText(wrongAnswer2);
+        flashcardQuestion.setText(question);
+        flashcardAnswer.setText(answer);
+        flashcardAnswer1.setText(wrongAnswer1);
+        flashcardAnswer2.setText(answer);
+        flashcardAnswer3.setText(wrongAnswer2);
+
+        if (requestCode == REQUEST_CODE) {
+            flashcardDatabase.insertCard(new Flashcard(question, answer, wrongAnswer1, wrongAnswer2));
+            allFlashcards = flashcardDatabase.getAllCards();
 
             Snackbar.make(findViewById(R.id.flashcardQuestion),
                     "Flashcard added!",
                     Snackbar.LENGTH_SHORT)
                     .show();
+        } else if (requestCode == EDIT_REQUEST_CODE) {
+            Flashcard cardToEdit = allFlashcards.get(currentCardDisplayedIndex);
+            cardToEdit.setQuestion(question);
+            cardToEdit.setAnswer(answer);
+            cardToEdit.setWrongAnswer1(wrongAnswer1);
+            cardToEdit.setWrongAnswer2(wrongAnswer2);
+            flashcardDatabase.updateCard(cardToEdit);
         }
     }
 }
